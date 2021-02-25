85bd7a618d85e044e7399b1e7a87654c0b800897
James Blash <j.w.blash@gmail.com>
2021-02-25 18:10:44 -0500

    Hillbot is born

---
+++ hillbot.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+HILLBOT_TOKEN = os.getenv('HILLBOT_TOKEN')
+BOOPLESNOOTS = os.getenv('BOOPLESNOOTS')
+CEDAR_RAPIDS = os.getenv('CEDAR_RAPIDS')
+
+hillbot = discord.Client()
+
+@hillbot.event
+async def on_ready():
+    for guild in hillbot.guilds:
+        print(
+            f'{hillbot.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})'
+        )
+
+@hillbot.event
+async def on_voice_state_update(member, before, after):
+        if before.channel is None and after.channel is not None:
+            if after.channel.name == 'Cedar Rapids':
+                await member.guild.system_channel.send(f'{member.name} is just chillin\' in Cedar Rapids.')
+                print(f'{member.name} is just chillin\' in Cedar Rapids.')
+
+hillbot.run(HILLBOT_TOKEN)
+
